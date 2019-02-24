2523f34c8940e855630a7ce37bcd7f16aa1d062d
gutsstas <tayson_1200@mail.ru>
2019-02-24 22:46:42 +0300

    feat: add new function for Spichonees

--- src/index.js
+++ src/index.js
@@ -3,5 +3,18 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  let count = 0;
+  for (let i = 0; i < preferences.length; i++) {
+      let next_one = preferences[i] - 1;
+      if (next_one == i) continue;
+      let next_two = preferences[next_one] - 1;
+      if (next_one == next_two) continue;
+      let next_three = preferences[next_two] - 1;
+      if (next_three == next_two) continue;
+      if (next_three == i) {
+          count++;
+      }
+   }
+   return count / 3;
+
 };
